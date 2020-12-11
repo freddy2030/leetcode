1549961f5f2a3cb9547c652a0d6dbdcdf00aaa5d
freddy <864242486@qq.com>
2020-12-11 12:35:25 +0800

    3n披萨

--- 3n披萨.py
+++ 3n披萨.py
@@ -8,23 +8,24 @@ def maxSizeSlices(slices):
     dp.append((slices, 0))
     while len(dp) > 0:
         slices, currentPiza = dp.pop()
-        for i, v in enumerate(slices):
-            t = deepcopy(slices)
-            if len(t) <= 3:
-                maxPiza = max(maxPiza, max(t) + currentPiza)
-            else:
+        if len(slices) <= 3:
+            maxPiza = max(maxPiza, (max(slices) + currentPiza))
+        else:
+            for i, v in enumerate(slices):
+                t = deepcopy(slices)
                 if i == (len(slices) - 1):
-                    del t[i-1], t[i-1], t[-1]
+                    del t[i-1], t[i-1], t[0]
                 elif i == 0:
                     del t[-1], t[0], t[0]
                 else:
                     del t[i-1], t[i-1], t[i-1]
                 dp.append((t, v+currentPiza))
-                maxPiza = max(maxPiza, currentPiza)
+                maxPiza = max(maxPiza, (v+currentPiza))
+        
     return maxPiza
 
 
 if __name__ == "__main__":
-    slices = [1,2,3,4,5,6]
+    slices = [9,5,1,7,8,4,4,5,5,8,7,7]
     res = maxSizeSlices(slices)
     print(res)
